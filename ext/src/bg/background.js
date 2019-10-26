40d927ba76e5e2667e318e0261fa08b5d1f1ca2d
Rebecca Wong <rebeccacwong@berkeley.edu>
2019-10-26 13:41:38 -0700

    ext changes

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -18,29 +18,55 @@
 
 // iterate through the links and set these variables
 
-var link = "usatoday.com"
-var iconHTML = "<img src = 'https://www.google.com/s2/favicons?domain=" + link + "' />";
-var title = "Default title"
-var snippet = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+var link = "http://usatoday.com";
+var iconHTML = "<img src = 'https://www.google.com/s2/favicons?domain=" + link.slice(7) + "' />";
+var title = "Default title";
+var snippet = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat";
 
 function stringDiv() {
 	string = "";
-	string += "<a href = " + link + ">"
+	string += "<a href = " + link + ">";
 	string += "<div class = 'social-card'>";
-	string += "<h2>" + title + "</h2>"
-	string += "<a href = " + link + ">" + iconHTML + '  ' + link + "</a>"
-	string += "<p>" + snippet + "</p>"
-	string += "</div> </a>"
-	console.log(string);
+	string += "<h2>" + title + "</h2>";
+	string += "<a href = " + link + ">" + iconHTML + '  ' + link + "</a>";
+	string += "<p>" + snippet + "</p>";
+	string += "</div> </a>";
 	return string;
-}
+};
 
 document.addEventListener('DOMContentLoaded', function() {
 
-	document.getElementById('stories').innerHTML = stringDiv();
+	document.getElementById('stories').innerHTML += stringDiv();
+	var socialCards = document.getElementsByClassName("social-card");
+
+	$( ".social-card").on("mouseenter", function() {
+	  	alert('mouseover');
+		this.addClass('social-card-hover');
+	});
+
+	document.getElementById('stories').innerHTML += stringDiv();
+	// this second one is just an example, delete later
 
 });
 
+chrome.browserAction.onClicked.addListener(function(tab) {
+    console.log('Injecting content script(s)');
+    //On Firefox document.body.textContent is probably more appropriate
+    chrome.tabs.executeScript(tab.id,{
+        code: 'document.body.innerText;'
+        //If you had something somewhat more complex you can use an IIFE:
+        //code: '(function (){return document.body.innerText})();'
+        //If your code was complex, you should store it in a
+        // separate .js file, which you inject with the file: property.
+    },receiveText);
+});
+
+//tabs.executeScript() returns the results of the executed script
+//  in an array of results, one entry per frame in which the script
+//  was injected.
+function receiveText(resultsArray){
+    console.log(resultsArray[0]);
+}
 
 
 
