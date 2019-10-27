84a9627849ef547ea4b2710d5bb1a01d2af38a60
Aniruddh Chennapragada <anic@berkeley.edu>
2019-10-27 05:28:16 -0700

    Resolved change conflict

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -1,3 +1,4 @@
+// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
 
 // var settings = new Store("settings", {
 //     "sample_setting": "This is how you use Store.js to remember values"
@@ -20,75 +21,87 @@
 
 var newsInfo = {}
 function getURL() {
-
+    
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var CURRENTURL = tabs[0].url;
-    console.log(CURRENTURL)
-    console.log('POST ajax called')
+        var CURRENTURL = tabs[0].url;
+        console.log(CURRENTURL)
+        console.log('POST ajax called')
 
-    $.ajax({
-        url: 'http://127.0.0.1:5000/send_url',
-        /*data: {u:str},*/
-        dataType: 'json',
-        data: JSON.stringify({u:CURRENTURL}),
-        type: 'POST',
-        contentType: 'application/json; charset=utf-8',
-        success: function(data) { //callback
-            console.log(data);
-            newsInfo = data
-            /*newsInfo = {"title": [], "provider": [], "description": []}*/
-            console.log("titles are: ", newsInfo["title"])
-            stringDiv(0)
-            stringDiv(1)
-            stringDiv(2)
-            load()
-        },
-        error: function(data){
-            console.log('ERROR');
-        },
-
+        $.ajax({    
+            url: 'http://127.0.0.1:5000/send_url',
+            /*data: {u:str},*/
+            dataType: 'json',
+            data: JSON.stringify({u:CURRENTURL}),
+            type: 'POST',
+            contentType: 'application/json; charset=utf-8',
+            success: function(data) { //callback
+                if (data["title"] == "Error") {
+                    console.log("Error CAUGHT")
+                    reportError()
+                    return
+                }
+                console.log(data);
+                newsInfo = data
+                /*newsInfo = {"title": [], "provider": [], "description": [], "URL": []}*/
+                console.log("titles are: ", newsInfo["title"])
+                load(2)
+            },
+            error: function(data){
+                console.log('ERROR');
+            },
+             
         });
-
     });
 
-    console.log("getURL finished")
-
-
-}
+    
+}   
 
 strings = []
 
-function stringDiv(i) {
-
-    console.log("stringDiv called")
-    var link = newsInfo["provider"][i];
-    var iconHTML = "<img src = 'https://www.google.com/s2/favicons?domain=" + link.slice(7) + "' />";
-    var title = newsInfo["title"][i];
-    var snippet = newsInfo["description"][i];
-    var btn = document.createElement('button')
-
-
-
-    string = "";
-	string += "<a href = " + link + ">";
-	string += "<div class = 'social-card'>";
-	string += "<h2>" + title + "</h2>";
-	string += "<a href = " + link + ">" + iconHTML + '  ' + link + "</a>";
-	string += "<p>" + snippet + "</p>";
-	string += "</div> </a>";
-    console.log("string generated")
-
-	strings.push(string)
-
+function reportError(){
+    string = "<p>" + "Are you sure you're reading good news?" + "</p>";
+    strings.push(string)
+    load(1)
 }
 
+function createDiv(i) {
 
-function load() {
+    while (i >= 0) {
+        var title = newsInfo["title"][i];
+        var provider = newsInfo["provider"][i];
+        var snippet = newsInfo["description"][i];
 
-    document.getElementById('stories').innerHTML += strings[0];
-    document.getElementById('stories').appendChild()
-    document.getElementById('stories').innerHTML += strings[1];
-    document.getElementById('stories').innerHTML += strings[2];
+        string = "";
+    	string += "<a href = \"" + provider + "\">";
+    	string += "<div id = 'apple' class = 'social-card' >";
+    	string += "<h2>" + title + "</h2>";
+    	string += "<a href = " + provider + ">" + provider + "</a>";
+    	string += "<p>" + snippet + "</p>";
+    	string += "</div> </a>";
+
+    	strings.push(string)
+        i -=1
+    }
+}
+
+function load(i) {
+    createDiv(i)
+    while (i > 0) {
+        document.getElementById('stories').innerHTML += strings[i];
+        console.log(i, i.toString())
+        console.log('"+i.toString()+"')
+        console.log("apple"+i.toString())
+        var link = document.getElementById('apple') 
+        var newlink = newsInfo["URL"][i]
+        link.addEventListener('click', function(){ 
+            chrome.tabs.create({url: newlink});
+        })
+        i -= 1
+        
+    }
+
+
+
 
     // this second one is just an example, delete later
 }
@@ -96,6 +109,7 @@ function load() {
 
 document.addEventListener('DOMContentLoaded', function() {
     getURL()
+
 });
 
 
